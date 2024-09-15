5a04e44beda19edd8d1d4f2f7e2cbf7d652deda9
Abdur Rahman <abdurrahmanbatavia@gmail.com>
2024-09-15 10:29:45 +0500

    chap 9 assignment 6 complete

--- chap 9-11/assignment6/app.js
+++ chap 9-11/assignment6/app.js
@@ -4,6 +4,37 @@ var percentage = obtainedMarks / totalMarks * 100;
 // alert(percentage+ "%")
 if (percentage>= 80) {
     document.write(
-        "Total marks :"+ " "
-        "Grade : A-one")
+        "Total marks :"+ " "+ totalMarks+
+        "<br> Obatained marks :" + " " +obtainedMarks 
+        + "<br> Percentage :" + " " + percentage+ "%" +
+        "<br> Grade : A-one"
+        + "<br> Remarks : Excellent"
+    )
+}
+else if (percentage>= 70) {
+    document.write(
+        "Total marks :"+ " "+ totalMarks+
+        "<br> Obatained marks :" + " " +obtainedMarks 
+        + "<br> Percentage :" + " " + percentage+ "%" +
+        "<br> Grade : A"
+        + "<br> Remarks : Good"
+    )
+}
+else if (percentage>= 60) {
+    document.write(
+        "Total marks :"+ " "+ totalMarks+
+        "<br> Obatained marks :" + " " +obtainedMarks 
+        + "<br> Percentage :" + " " + percentage+ "%" +
+        "<br> Grade : B"
+        + "<br> Remarks : You need to improve"
+    )
+}
+else if (percentage < 60) {
+    document.write(
+        "Total marks :"+ " "+ totalMarks+
+        "<br> Obatained marks :" + " " +obtainedMarks 
+        + "<br> Percentage :" + " " + percentage+ "%" +
+        "<br> Grade : Fail"
+        + "<br> Remarks : Sorry"
+    )
 }
