ff174d090102e5856cdeb27e547ab666f0bc674a
Abdur Rahman <abdurrahmanbatavia@gmail.com>
2024-09-15 10:41:25 +0500

    assginment 8 and 9 done

---
+++ chap 9-11/assignment8/app.js
@@ -0,0 +1,6 @@
+var number = prompt("enter a number")
+var check = number%3;
+if (check==0)
+    {
+        alert("number is divisible by 3")
+    }
---
+++ chap 9-11/assignment9/app.js
@@ -0,0 +1,15 @@
+var number = prompt("enter a number")
+var check = number%2;
+if (check==0)
+    {
+        alert("number is even")
+    }
+// else if (check=="")
+//     {
+//         alert("number is not entered")
+//     }
+else
+    {
+        alert("number is odd")
+    }
+
