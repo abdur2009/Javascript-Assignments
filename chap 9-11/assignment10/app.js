6513110f422a465b763c437cb31b4a3e0d59ae2d
abdur2009 <abdurrahmanbatavia@gmail.com>
2024-09-15 10:59:20 +0500

    Chap 9-11 assignments completed

---
+++ chap 9-11/assignment10/app.js
@@ -0,0 +1,17 @@
+var temp = prompt("whats the temperature today")
+if (temp>40)
+{
+    alert("its too hot out side")
+}
+else if (temp>30)
+{
+    alert("The weather today is normal")
+}
+else if (temp>20)
+{
+    alert("Todays weather is cool")
+}
+else if (temp>10)
+{
+    alert("Todays weather is so cold")
+}
---
+++ chap 9-11/assignment11/app.js
@@ -0,0 +1,34 @@
+var num1 = Number(prompt("enter your first number"))
+var num2 = Number(prompt("enter your second number"))
+var operation = prompt("choose operation +,-,*,/,%")
+
+if (operation=="+")
+{
+var result = num1+num2
+alert(result)
+
+}
+if (operation=="-")
+{
+var result = num1-num2
+alert(result)
+
+}
+if (operation=="*")
+{
+var result = num1*num2
+alert(result)
+
+}
+if (operation=="/")
+{
+var result = num1/num2
+alert(result)
+
+}
+if (operation=="%")
+{
+var result = num1%num2
+alert(result)
+
+}
